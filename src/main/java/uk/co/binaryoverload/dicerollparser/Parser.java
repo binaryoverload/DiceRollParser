52a97780379489b1baff52decae928e72b72f318
William Oldham <wegg7250@gmail.com>
2021-01-22 20:36:45 +0000

    Improve and comment regex

--- src/main/java/uk/co/binaryoverload/dicerollparser/Parser.java
+++ src/main/java/uk/co/binaryoverload/dicerollparser/Parser.java
@@ -20,11 +20,29 @@ import java.util.stream.Stream;
 
 public class Parser {
 
-    private static final Pattern ROLL_PATTERN = Pattern.compile("(?:\\(?)((?:\\d+d\\d+,?)+)(?:\\)?)(?:((?:k|p|mi|ma|rr|ro|ra|e)[><lh]?\\d+)+)?(?:\\[(.+)\\])?((?:[+\\-*\\/]\\d+)+(?!d))?([+\\-*\\/])?");
+    private static final Pattern ROLL_PATTERN =
+            //@formatter:off
+            Pattern.compile(
+                    "(?:" +
+                            "(\\d+d\\d+)" + // Capture Group 1, matches a single instance of 1d20
+                            "|" + // or
+                            "\\(((?:\\d+d\\d+,?)+)\\)" + // Capture Group 2, matches a CSV list of 1d20,2d20 instead ()
+                    ")" +
+                    "(" + // Capture Group 3 (Is optional), matches a single or multiple selectors
+                        "(?:" +
+                            "(?:k|p|mi|ma|rr|ro|ra|e)" + // Modifier Type
+                            "(?:\\d+)?" + // Modifier Value (Optional)
+                            "(?:[><lh]\\d+)?" + // Selector and value (Optional)
+                        ")+" + // One or more selectors
+                    ")?" +
+                    "((?:[+\\-*\\/]\\d+)+(?!d))?" + // Capture Group 4 (Is optional), matches a list of operators (+4*3)
+                    "(?:\\[(.+)\\])?"+ // Capture Group 5 (Is optional), matches a [label] with any text inside
+                    "(?:([+\\-*\\/])|$)"); // Capture Group 6, matches the operator between dice rolls
+            //@formatter:off
 
     private static final Pattern ADDED_VALUE_PATTERN = Pattern.compile("([+\\-*\\/])(\\d+)");
     private static final Pattern DICE_PATTERN = Pattern.compile("^(\\d+)d(\\d+)$");
-    private static final Pattern MODIFIER_PATTERN = Pattern.compile("(k|p|mi|ma|rr|ro|ra|e)(\\d+)?(?:([><lh])?([0-9]+))?");
+    private static final Pattern MODIFIER_PATTERN = Pattern.compile("(k|p|mi|ma|rr|ro|ra|e)(\\d+)?(?:([><lh])(\\d+))?");
 
     public static void main(String[] args) {
 
@@ -34,7 +52,7 @@ public class Parser {
                 "2d20kh1+4",
                 "2d20kl1-2",
                 "4d6ro<3",
-                "2d6e6",
+                "2d6e6e10",
                 "4d6mi2[fire]+2d20kh1+4+7+11+3d10ra10",
                 "(1d6,1d8,1d10)kh2"
         );
@@ -43,41 +61,53 @@ public class Parser {
 
             System.out.println("\n ---- New Input ---- \n");
 
-            Matcher rollMatcher = ROLL_PATTERN.matcher(inputRoll);
-
-            List<DiceRoll> rolls = new ArrayList<>();
-            while (rollMatcher.find()) {
-                String[] diceCombos = rollMatcher.group(1).split("\\s*,\\s*");
-                List<Die> dice = parseDie(diceCombos);
-
-
-                Stream<MatchResult> modifiersInput = Stream.of();
-                if (rollMatcher.group(2) != null) {
-                    modifiersInput = MODIFIER_PATTERN.matcher(rollMatcher.group(2)).results();
-                }
-                List<Modifier> modifiers = parseModifiers(modifiersInput);
-
-                String label = rollMatcher.group(3);
-
-                Stream<MatchResult> addedValueInput = Stream.of();
-                if (rollMatcher.group(4) != null) {
-                    addedValueInput = ADDED_VALUE_PATTERN.matcher(rollMatcher.group(4)).results();
-                }
-                List<AddedValue> addedValues = parseAddedValues(addedValueInput);
-
-                Operator operator = null;
-                if (rollMatcher.group(5) != null) {
-                    operator = Operator.getLiteralCache().get(rollMatcher.group(5));
-                }
-
-                DiceRoll roll = new DiceRoll(dice, label, modifiers, addedValues, operator);
-                rolls.add(roll);
-                System.out.println(roll);
-            }
-
+            List<DiceRoll> rolls = parseDiceRoll(inputRoll);
+            rolls.forEach(System.out::println);
         }
 
 
+    }
+
+    public static List<DiceRoll> parseDiceRoll(String input) {
+        Matcher rollMatcher = ROLL_PATTERN.matcher(input);
+
+        List<DiceRoll> rolls = new ArrayList<>();
+        while (rollMatcher.find()) {
+
+            String[] diceCombos = new String[0];
+            if (rollMatcher.group(1) != null) {
+                diceCombos = new String[]{rollMatcher.group(1)};
+            } else if (rollMatcher.group(2) != null) {
+                diceCombos = rollMatcher.group(2).split("\\s*,\\s*");
+            } else {
+                throw new RuntimeException("No dice could be parsed!");
+            }
+
+            List<Die> dice = parseDie(diceCombos);
+
+            Stream<MatchResult> modifiersInput = Stream.of();
+            if (rollMatcher.group(3) != null) {
+                modifiersInput = MODIFIER_PATTERN.matcher(rollMatcher.group(3)).results();
+            }
+            List<Modifier> modifiers = parseModifiers(modifiersInput);
+
+
+            Stream<MatchResult> addedValueInput = Stream.of();
+            if (rollMatcher.group(4) != null) {
+                addedValueInput = ADDED_VALUE_PATTERN.matcher(rollMatcher.group(4)).results();
+            }
+            List<AddedValue> addedValues = parseAddedValues(addedValueInput);
+
+            String label = rollMatcher.group(5);
+
+            Operator operator = null;
+            if (rollMatcher.group(6) != null) {
+                operator = Operator.getLiteralCache().get(rollMatcher.group(6));
+            }
+
+            rolls.add(new DiceRoll(dice, label, modifiers, addedValues, operator));
+        }
+        return rolls;
     }
 
     private static List<AddedValue> parseAddedValues(Stream<MatchResult> addedValueInput) {
